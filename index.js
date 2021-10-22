3f05e01d74cd173f40029638d1bf010c9f3c4e05
Max <90480680+MaxGarciaD@users.noreply.github.com>
2021-10-22 13:01:39 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/pulse/bootstrap.min.css"
-    />
-  </head>
-  <body>
-    <div class="container p-4">
-      <div class="row">
-        <div class="col-md-4 offset-md-4">
-          <div class="card">
-            <div class="card-body">
-              <form id="task-form">
-                <div class="form-group">
-                  <input
-                    type="text"
-                    id="task-title"
-                    class="form-control"
-                    placeholder="Task Title"
-                    autofocus
-                  />
-                </div>
-                <div class="form-group">
-                  <textarea
-                    id="task-description"
-                    rows="3"
-                    class="form-control"
-                    placeholder="Task Description"
-                  ></textarea>
-                </div>
-                <button
-                  type="submit"
-                  class="btn btn-primary"
-                  id="btn-task-form"
-                >
-                  Save
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    <script type="module" src="main.js"></script>
-    <script type="module" src="index.js"></script>
-  </body>
-</html>
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMV5Mig3YV6ivWM4ltnSNG7pWEL1Ia5qg",
+  authDomain: "lmgd-web-crud.firebaseapp.com",
+  projectId: "lmgd-web-crud",
+  storageBucket: "lmgd-web-crud.appspot.com",
+  messagingSenderId: "951354016324",
+  appId: "1:951354016324:web:fbd9ba89409b61c67eb211"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
